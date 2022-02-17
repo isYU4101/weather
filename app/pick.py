26ed480b3aaa6d6d5694b2ffc9375d2c852519a3
yuma <yuma131014@gmail.com>
2022-02-17 22:42:01 +0900

    まとめの一通りの機能実装

--- app/pick.py
+++ app/pick.py
@@ -14,37 +14,56 @@ def pick_up_data():     # ３サイトのデータを返す
         "https://tenki.jp/forecast/10/50/9110/47201/", 
         "https://weather.goo.ne.jp/weather/division-1/471010/"
     ]
+
     data = {
         "yahoo": {
-            "wether": None, "high": None, "low": None
+            "weather": None, "high": None, "low": None
         },
         "tenki_jp": {
-            "wether": None, "high": None, "low": None
+            "weather": None, "high": None, "low": None
         },
         "goo": {
-            "wether": None, "high": None, "low": None
-        }
+            "weather": None, "high": None, "low": None
+        },
+        "total": {
+            "weather": None, "high": None, "low": None
+        },
     }
+
     # yahooの情報
     html = pick_up(target_url[0])
     div_site_detail = html.find('div', class_="forecastCity")
-    data["yahoo"]["wether"] = div_site_detail.find('p', class_="pict").get_text().replace("\n", "").replace(" ", "")
-    data["yahoo"]["high"] = div_site_detail.find('li', class_="high").get_text()[0:2]
-    data["yahoo"]["low"] = div_site_detail.find('li', class_="low").get_text()[0:2]
+    data["yahoo"]["weather"] = div_site_detail.find('p', class_="pict").get_text().replace("\n", "").replace(" ", "")
+    data["yahoo"]["high"] = int(div_site_detail.find('li', class_="high").get_text()[0:2])
+    data["yahoo"]["low"] = int(div_site_detail.find('li', class_="low").get_text()[0:2])
 
     # tenki.jpの情報
     html = pick_up(target_url[1])
     div_site_detail = html.find('div', class_="forecast-days-wrap")
-    data["tenki_jp"]["wether"] = div_site_detail.find('p', class_="weather-telop").get_text().replace("\n", "").replace(" ", "")
-    data["tenki_jp"]["high"] = div_site_detail.find('dd', class_="high-temp temp").get_text()[0:2]
-    data["tenki_jp"]["low"] = div_site_detail.find('dd', class_="low-temp temp").get_text()[0:2]
+    data["tenki_jp"]["weather"] = div_site_detail.find('p', class_="weather-telop").get_text().replace("\n", "").replace(" ", "")
+    data["tenki_jp"]["high"] = int(div_site_detail.find('dd', class_="high-temp temp").get_text()[0:2])
+    data["tenki_jp"]["low"] = int(div_site_detail.find('dd', class_="low-temp temp").get_text()[0:2])
 
     # ウェザーニュースの情報
     html = pick_up(target_url[2])
     div_site_detail = html.find_all('div', class_="weather_area weather")[0]
-    data["goo"]["wether"] = div_site_detail.find('p', class_="weather").get_text()
-    data["goo"]["high"] = div_site_detail.find('p', class_="red").get_text()[7:9]
-    data["goo"]["low"] = div_site_detail.find('p', class_="blue").get_text()[7:9]
-    return data
+    data["goo"]["weather"] = div_site_detail.find('p', class_="weather").get_text()
+    data["goo"]["high"] = int(div_site_detail.find('p', class_="red").get_text()[7:9])
+    data["goo"]["low"] = int(div_site_detail.find('p', class_="blue").get_text()[7:9])
 
-print(pick_up_data())
+    # まとめの情報
+    data["total"]["high"] = int((data["yahoo"]["high"] + data["tenki_jp"]["high"] + data["goo"]["high"]) / 3)
+    data["total"]["low"] = int((data["yahoo"]["low"] + data["tenki_jp"]["low"] + data["goo"]["low"]) / 3)
+
+    text = data["yahoo"]["weather"] + data["tenki_jp"]["weather"] + data["goo"]["weather"]
+    hare = text.count("晴")
+    ame = text.count("雨")
+    kumo = text.count("曇")
+    if (hare > ame) and (hare > kumo):
+        data["total"]["weather"] = "晴れ"
+    elif (ame > hare) and (ame > kumo):
+        data["total"]["weather"] = "雨"
+    else:
+        data["total"]["weather"] = "曇り"
+    
+    return data
--- app/views.py
+++ app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .pick import pick_up_data
+import datetime
 
 def index(request):
-    return render(request, 'app/index.html')
+    dt_now = str(datetime.datetime.now())
+    month = dt_now[5:7]
+    day = dt_now[8:10]
+    data = pick_up_data()["total"]
+    params = {
+        'month': month,
+        'day': day,
+        'weather': data["weather"],
+        'high': data["high"],
+        'low': data["low"]
+    }
+    return render(request, 'app/index.html', params)
 
 # Create your views here.
